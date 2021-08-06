0deb43e16644f8e5ed267f076e3fde31e37f3663
Stinky Ball <74356284+Kayal314@users.noreply.github.com>
2021-08-06 10:33:02 +0530

    Update charge.py

--- charge.py
+++ charge.py
@@ -3,8 +3,11 @@ import math
 
 class Charge:
     def __init__(self, **kwargs):
-        self.magnitude = kwargs.get('magnitude') * 10e5
+        self.magnitude = kwargs.get('magnitude')
         self.position = kwargs.get('position')
+
+    def get_sign(self):
+        return self.magnitude/math.fabs(self.magnitude)
 
     def get_sq_distance_from(self, position: tuple):
         return (self.position[0] - position[0]) ** 2 + (self.position[1] - position[1]) ** 2
@@ -13,7 +16,13 @@ class Charge:
         if self.position[0] == position[0]:
             return 0, 1
         slope = (self.position[1] - position[1]) / (self.position[0] - position[0])
-        angle = math.atan(slope)
+        angle = math.fabs(math.atan(slope))
+        if self.position[0] > position[0] and self.position[1] > position[1]:
+            angle = math.pi - angle
+        elif self.position[0] > position[0] and self.position[1] < position[1]:
+            angle = math.pi + angle
+        elif self.position[0] < position[0] and position[1] > self.position[1]:
+            angle = -angle
         return math.cos(angle), math.sin(angle)
 
     def get_shifted_position(self):
